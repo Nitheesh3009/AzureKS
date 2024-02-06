73d21edf4a235ee70b5bfb1573d8ee9ccf831ad3
Nitheesh <Nitheeshedla@my.unt.edu>
2024-02-06 10:31:50 -0600

    added services files for key vault

---
+++ Services/KeyVault/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "rgname" {
+  type = string
+}
